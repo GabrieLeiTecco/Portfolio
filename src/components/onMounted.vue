de9835d21abdd54035e00dba5d9f4163c3f5e145
Gabriel Leite Tecco <111099300+GabrieLeiTecco@users.noreply.github.com>
2024-07-31 21:17:00 -0300

    Create onMounted.vue

---
+++ src/components/onMounted.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+var timer = 0
+onMounted(()=>{
+  setTimeout(()=>{
+    timer++
+  }, 1000)
+})
+</script>
+
+<template>
+  <div>
+    {{timer}}
+  </div>
+</template>
